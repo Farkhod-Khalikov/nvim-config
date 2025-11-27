2b46f6f2d176b3b9adde850f48b86d1024f2fc8a
Farkhod Khalikov <farkhod.khalikov.main@gmail.com>
2025-11-27 16:02:53 +0500

    Add nvim-cmp autocompletion

--- init.lua
+++ init.lua
@@ -8,10 +8,14 @@ require("config.autocmd")
 -- 2. configure/learn telescope for searching, find and replace, + macros 
 -- 3. harpoon 
 
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
 -- configure lsp (the lsp shoulbe installed On the machine manually like: "npm install -g typescript-language-server") 
 vim.lsp.config('ts_ls', {
   cmd = { 'typescript-language-server', '--stdio' },
   filetypes = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+  capabilities = capabilities,
 })
 
 -- enable it 
---
+++ lua/plugins/autocompletion.lua
@@ -0,0 +1,14 @@
+
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  config = function()
+    local cmp = require("cmp")
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert(),
+      sources = { { name = "nvim_lsp" } },
+    })
+  end
+}
